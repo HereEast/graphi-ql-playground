7d281391f4af9db684f13eec31ae28b40c35c02e
Here East <nastiapiven@gmail.com>
2023-12-05 19:27:37 +0100

    feat: add language select to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,41 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState, ChangeEvent } from "react";
 import Link from "next/link";
 import { Page } from "../../types";
+import { LANGS } from "../../utils/constants";
 
+import classnames from "classnames";
 import styles from "./header.module.scss";
 
 function Header(): ReactElement {
+  const [isScroll, setIsScroll] = useState(false);
+  const [lang, setLang] = useState("");
+
+  useEffect(() => {
+    const savedLang = localStorage.getItem("lang") || "";
+    setLang(savedLang);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    });
+  }, []);
+
+  function handleSelectLang(e: ChangeEvent<HTMLSelectElement>): void {
+    const selectedLang = e.target.value;
+
+    setLang(selectedLang);
+    localStorage.setItem("lang", selectedLang);
+  }
+
   return (
-    <header className={styles.header}>
+    <header className={classnames(styles.header, isScroll && styles.header__blue)}>
       <nav className={styles.nav}>
-        <Link href={Page.HOME} className={styles.nav__logo}>
-          GraphiQL Playground
-        </Link>
+        <Link href={Page.HOME} className={styles.nav__logo}></Link>
         <ul className={styles.nav__links}>
           <li>
             <Link href={Page.LOGIN} className={styles.link}>
@@ -23,7 +48,15 @@ function Header(): ReactElement {
             </Link>
           </li>
         </ul>
-        <div className={styles.nav__lang}>LANG</div>
+        <div className={styles.nav__lang}>
+          <select className={styles.select} value={lang} onChange={handleSelectLang}>
+            {LANGS.map((option) => (
+              <option key={option} value={option}>
+                {option.toUpperCase()}
+              </option>
+            ))}
+          </select>
+        </div>
       </nav>
     </header>
   );
