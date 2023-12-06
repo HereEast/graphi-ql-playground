22472ed3e7449386f1691ce2de731e87cbf6e7fb
Here East <nastiapiven@gmail.com>
2023-12-06 11:44:54 +0100

    feat: create ProtectedRoutte component

---
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,22 @@
+import { ReactElement, ReactNode, useEffect } from "react";
+import { useRouter } from "next/router";
+import { IS_AUTH } from "../../utils/constants";
+import { Page } from "../../types";
+
+export interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+function ProtectedRoute({ children }: ProtectedRouteProps): ReactElement | null {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!IS_AUTH) {
+      router.replace(Page.LOGIN);
+    }
+  }, [router]);
+
+  return IS_AUTH ? <>{children}</> : null;
+}
+
+export default ProtectedRoute;
