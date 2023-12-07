168b4c19108f60c82715952895f0ab43633c8c4d
Here East <nastiapiven@gmail.com>
2023-12-07 21:34:35 +0100

    feat: add validation for Login form

--- src/components/_pages/Login/Login.tsx
+++ src/components/_pages/Login/Login.tsx
@@ -1,12 +1,33 @@
 import Link from "next/link";
 import { ReactElement } from "react";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Input } from "../../Input";
 import { Button } from "../../Button";
-import { ButtonName, Page } from "../../../types";
+import { ButtonName, Page, ILoginFormData } from "../../../types";
+import { loginValidationSchema } from "../../../utils";
 
 import styles from "./login.module.scss";
 
 function Login(): ReactElement {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(loginValidationSchema),
+  });
+
+  function onSubmit(data: ILoginFormData): void {
+    console.log(data);
+
+    router.replace(Page.HOME);
+  }
+
   return (
     <div className={styles.login}>
       <div className={styles.login__container}>
@@ -16,10 +37,17 @@ function Login(): ReactElement {
             Please, log in to use GraphiQL Playground.
           </p>
         </div>
-        <form className={styles.login__form}>
-          <Input inputName="Email" type="email" />
-          <Input inputName="Password" type="text" />
-          <Button name={ButtonName.LOGIN} className={styles.login__form_button} />
+        <form className={styles.login__form} onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.login__form_inputs}>
+            <Input inputName="email" type="text" errors={errors} {...register("email")} />
+            <Input inputName="password" type="text" errors={errors} {...register("password")} />
+          </div>
+          <Button
+            name={ButtonName.LOGIN}
+            type="submit"
+            className={styles.login__form_button}
+            disabled={isSubmitting}
+          />
         </form>
       </div>
       <div className={styles.login__footer}>
