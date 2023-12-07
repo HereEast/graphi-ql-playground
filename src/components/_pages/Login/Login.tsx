117203fdf2bc6befeaa1aa81ddf70b28f48dab57
Here East <nastiapiven@gmail.com>
2023-12-07 01:31:38 +0100

    feat: create Login page component

--- src/components/_pages/Login/Login.tsx
+++ src/components/_pages/Login/Login.tsx
@@ -1,7 +1,34 @@
+import Link from "next/link";
 import { ReactElement } from "react";
+import { Input } from "../../Input";
+import { Button } from "../../Button";
+import { ButtonName, Page } from "../../../types";
+
+import styles from "./login.module.scss";
 
 function Login(): ReactElement {
-  return <h1>Login</h1>;
+  return (
+    <div className={styles.login}>
+      <div className={styles.login__container}>
+        <div className={styles.login__header}>
+          <h2 className={styles.login__header_title}>Hello again 👋</h2>
+          <p className={styles.login__header_subtitle}>
+            Please, log in to use GraphiQL Playground.
+          </p>
+        </div>
+        <form className={styles.login__form}>
+          <Input inputName="Email" type="email" />
+          <Input inputName="Password" type="text" />
+          <Button name={ButtonName.LOGIN} className={styles.login__form_button} />
+        </form>
+      </div>
+      <div className={styles.login__footer}>
+        <span>
+          Don't have an account? Please, <Link href={Page.REGISTER}>register.</Link>
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
