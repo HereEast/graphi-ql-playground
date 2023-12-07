1e2de04e6318233adc6952e396966b0403c348a5
Here East <nastiapiven@gmail.com>
2023-12-07 21:34:48 +0100

    feat: add validation for Register form

--- src/components/_pages/Register/Register.tsx
+++ src/components/_pages/Register/Register.tsx
@@ -1,12 +1,33 @@
 import Link from "next/link";
 import { ReactElement } from "react";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Input } from "../../Input";
 import { Button } from "../../Button";
-import { ButtonName, Page } from "../../../types";
+import { ButtonName, Page, IRegisterFormData } from "../../../types";
+import { registerValidationSchema } from "../../../utils";
 
 import styles from "./register.module.scss";
 
 function Register(): ReactElement {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(registerValidationSchema),
+  });
+
+  function onSubmit(data: IRegisterFormData): void {
+    console.log(data);
+
+    router.replace(Page.HOME);
+  }
+
   return (
     <div className={styles.register}>
       <div className={styles.register__container}>
@@ -16,11 +37,18 @@ function Register(): ReactElement {
             Please, create account to use GraphiQL Playground.
           </p>
         </div>
-        <form className={styles.register__form}>
-          <Input inputName="Name" type="text" />
-          <Input inputName="Email" type="email" />
-          <Input inputName="Password" type="text" />
-          <Button name={ButtonName.REGISTER} className={styles.register__form_button} />
+        <form className={styles.register__form} onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.register__form_inputs}>
+            <Input inputName="name" type="text" errors={errors} {...register("name")} />
+            <Input inputName="email" type="text" errors={errors} {...register("email")} />
+            <Input inputName="password" type="text" errors={errors} {...register("password")} />
+          </div>
+          <Button
+            name={ButtonName.REGISTER}
+            type="submit"
+            className={styles.register__form_button}
+            disabled={isSubmitting}
+          />
         </form>
       </div>
       <div className={styles.register__footer}>
