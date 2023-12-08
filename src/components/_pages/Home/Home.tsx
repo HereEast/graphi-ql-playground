9857fcdcf856ff22de40ae90b2d148d5ed8431a8
Here East <nastiapiven@gmail.com>
2023-12-08 19:34:20 +0100

    feat: add sections to Home page

--- src/components/_pages/Home/Home.tsx
+++ src/components/_pages/Home/Home.tsx
@@ -1,7 +1,25 @@
 import { ReactElement } from "react";
+import { HeroSection } from "../../_sections/HeroSection";
+import { TeamSection } from "../../_sections/TeamSection";
+import { CourseSection } from "../../_sections/CourseSection";
+
+import styles from "./home.module.scss";
 
 function Home(): ReactElement {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <HeroSection />
+
+      <section className={styles.image}>
+        <div className={styles.image__container}>
+          <div className={styles.image__img}></div>
+        </div>
+      </section>
+
+      <CourseSection />
+      <TeamSection />
+    </>
+  );
 }
 
 export default Home;
