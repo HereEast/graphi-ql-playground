4bade7d306dacf3528624bb6b1ffcf0333cb283e
Here East <nastiapiven@gmail.com>
2023-12-09 23:44:32 +0100

    refactor: refactor constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,57 +1,6 @@
-export const LANGS = ["en", "ru"];
+export * from "./team";
+export * from "./dictionary";
 
+export const LANGS = ["en", "ru"];
 export const LINK_SCHOOL = "https://rs.school/";
 export const LINK_COURSE = "https://rs.school/react/";
-
-export const IS_AUTH = false;
-
-export const TEAM = [
-  {
-    name: "Andrei Niasmachny",
-    image: "/images/Andrei.jpg",
-    github: "https://github.com/Andrew-Nes",
-    description:
-      "The course runs for a total of 10 weeks, with 6 weeks dedicated to studying React and an additional 4 weeks for the final task implementation.",
-    position: "Frontend Developer",
-  },
-  {
-    name: "Mikita Razumau",
-    image: "/images/Nikita.jpg",
-    github: "https://github.com/razumaumikita",
-    description:
-      "The course runs for a total of 10 weeks, with 6 weeks dedicated to studying React and an additional 4 weeks for the final task implementation.",
-    position: "Frontend Developer",
-  },
-  {
-    name: "Nastia Piven",
-    image: "/images/Nastia.jpg",
-    github: "https://github.com/HereEast",
-    description:
-      "The course runs for a total of 10 weeks, with 6 weeks dedicated to studying React and an additional 4 weeks for the final task implementation.",
-    position: "Frontend Developer",
-  },
-];
-
-export const COURSE_DATA = [
-  {
-    title: "Components",
-  },
-  {
-    title: "Routing",
-  },
-  {
-    title: "Tests, Context API",
-  },
-  {
-    title: "Redux. RTK",
-  },
-  {
-    title: "NextJS, SSR/SSG",
-  },
-  {
-    title: "Forms",
-  },
-  {
-    title: "Technical React Interview, GraphiQL",
-  },
-];
