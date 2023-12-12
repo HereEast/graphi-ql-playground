2dbef0fec5eee80f87c14bbf89714bdb7becc2d5
Here East <nastiapiven@gmail.com>
2023-12-12 23:26:56 +0100

    feat: refactor Button (add children props)

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,14 @@
-import { ReactElement, MouseEvent } from "react";
+import { ReactElement, MouseEvent, ReactNode } from "react";
 
 import clsx from "clsx";
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
   type?: "submit" | "reset" | "button";
-  name: string | undefined;
+  name?: string | undefined;
   className?: string;
   disabled?: boolean;
+  children?: ReactNode;
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
@@ -16,6 +17,7 @@ function Button({
   type = "button",
   className,
   disabled,
+  children,
   onClick = (): void => {},
 }: ButtonProps): ReactElement {
   return (
@@ -25,7 +27,8 @@ function Button({
       onClick={(event): void => onClick(event)}
       disabled={disabled}
     >
-      {name}
+      {name && name}
+      {children && children}
     </button>
   );
 }
