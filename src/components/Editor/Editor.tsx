68bf128450e189faef52b001cac6e63ecc3149da
Here East <nastiapiven@gmail.com>
2023-12-13 14:11:10 +0100

    feat: create Editor component

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,46 +1,75 @@
-import { ReactElement, useState, ChangeEvent } from "react";
-import { Button } from "..";
+import {
+  ReactElement,
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+  useRef,
+  UIEvent,
+  KeyboardEvent,
+} from "react";
 
+import clsx from "clsx";
 import styles from "./Editor.module.scss";
 
 export interface EditorProps {
   mode?: "edit" | "read";
+  code: string;
+  setCode?: Dispatch<SetStateAction<string>>;
 }
 
-function Editor({ mode }: EditorProps): ReactElement {
-  if (mode === "edit") console.log(mode);
-
-  const [code, setCode] = useState("");
+function Editor({ mode, code, setCode }: EditorProps): ReactElement {
+  const linesCountRef = useRef<HTMLUListElement>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
   function handleInput(e: ChangeEvent<HTMLTextAreaElement>): void {
-    setCode(e.target.value);
+    if (setCode) {
+      setCode(e.target.value);
+    }
+  }
+
+  function handleKeyDown(e: KeyboardEvent<HTMLTextAreaElement>): void {
+    if (!(e.target instanceof HTMLTextAreaElement)) return;
+
+    console.log(e.key);
+    console.log("Enter", e.key === "Enter");
+  }
+
+  function handleScroll(e: UIEvent<HTMLUListElement | HTMLTextAreaElement>): void {
+    if (!(e.target instanceof HTMLUListElement) || !(e.target instanceof HTMLTextAreaElement))
+      return;
+
+    if (linesCountRef.current) {
+      linesCountRef.current.scrollTop = e.target?.scrollTop;
+    }
+
+    if (textAreaRef.current) {
+      textAreaRef.current.scrollTop = e.target?.scrollTop;
+    }
   }
 
   return (
-    <div className={styles.editor}>
+    <div className={clsx(styles.editor, mode === "read" && styles.editor__response)}>
       <div className={styles.editor__container}>
         <div className={styles.editor__main}>
           {/* Lines */}
-          <ul className={styles.editor__linesCount}>
+          <ul className={styles.editor__linesCount} ref={linesCountRef} onScroll={handleScroll}>
             {code.split("\n").map((line, index) => (
-              <li key={`${line}-${index}`}>
+              <li key={`${line}-${index}`} className={styles.editor__linesCount_item}>
                 <span>{index + 1}</span>
               </li>
             ))}
           </ul>
-          <textarea onChange={handleInput} className={styles.editor__textArea} value={code} />
-          {/* Buttons */}
-          <div className={styles.editor__buttons}>
-            <Button className={styles.editor__buttons_request}>
-              <span>R</span>
-            </Button>
-            <Button className={styles.editor__buttons_prettify}>
-              <span>P</span>
-            </Button>
-          </div>
+          <textarea
+            onKeyDown={handleKeyDown}
+            onChange={handleInput}
+            className={styles.editor__textArea}
+            value={code}
+            onScroll={handleScroll}
+            ref={textAreaRef}
+            disabled={mode === "read"}
+            placeholder={mode === "read" ? "Place for response..." : ""}
+          />
         </div>
-        {/* Variables, Headers */}
-        <div className={styles.editor__options}></div>
       </div>
     </div>
   );
