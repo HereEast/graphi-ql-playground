3805548fe9cd185f49a32343efcefdc8e6900eaf
Here East <nastiapiven@gmail.com>
2023-12-13 13:51:15 +0100

    feat: add ResponseView and RequestView to Playground component

--- src/components/_pages/Playground/Playground.tsx
+++ src/components/_pages/Playground/Playground.tsx
@@ -1,7 +1,18 @@
 import { ReactElement } from "react";
+import { RequestView, ResponseView } from "../..";
+
+import styles from "./Playground.module.scss";
 
 function Playground(): ReactElement {
-  return <h1>Playground</h1>;
+  return (
+    <section className={styles.playground}>
+      <h1>Playground</h1>
+      <div className={styles.playground__views}>
+        <RequestView />
+        <ResponseView />
+      </div>
+    </section>
+  );
 }
 
 export default Playground;
