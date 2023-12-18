108304c05af68c87747713ad08f8d0bda15bc633
Here East <nastiapiven@gmail.com>
2023-12-18 10:30:25 +0100

    refactor: refactor code

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,12 +1,12 @@
 import "@testing-library/jest-dom";
 
-// import { render } from "@testing-library/react";
-// import { Button } from "..";
+import { render } from "@testing-library/react";
+import { Button } from "..";
 
 describe("Button component", () => {
   test("should be rendered with correct props", () => {
     expect(true).toBe(true);
 
-    // render(<Button name="Click" />);
+    render(<Button name="Click" />);
   });
 });
