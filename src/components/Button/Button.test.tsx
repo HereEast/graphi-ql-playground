58bfc85bff47928c45587a068ec7492e25d8db91
Here East <nastiapiven@gmail.com>
2023-12-18 12:12:59 +0100

    feat: setup jest

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -3,6 +3,12 @@ import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import { Button } from "..";
 
+jest.mock("../../services/firebase", () => ({
+  auth: {
+    getAuth: jest.fn(),
+  },
+}));
+
 describe("Button component", () => {
   test("should be rendered with correct props", () => {
     expect(true).toBe(true);
