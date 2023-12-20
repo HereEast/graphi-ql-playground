539579ad92dfc7e5f88cc13f7e0cad5e369556ca
Here East <nastiapiven@gmail.com>
2023-12-20 15:22:01 +0100

    feat: add test for ErrorMessage

---
+++ src/components/ErrorMessage/ErrorMessage.test.tsx
@@ -0,0 +1,25 @@
+import "@testing-library/jest-dom";
+
+import { render, screen } from "@testing-library/react";
+import { ErrorMessage } from "..";
+
+jest.mock("../../services/firebase", () => ({
+  auth: {
+    getAuth: jest.fn(),
+  },
+}));
+
+describe("Button component", () => {
+  test("should be rendered with correct props", () => {
+    const props = {
+      message: "Error message",
+      className: "error",
+    } as const;
+
+    render(<ErrorMessage {...props} />);
+
+    const error = screen.getByText(/Error message/i);
+
+    expect(error).toBeInTheDocument();
+  });
+});
