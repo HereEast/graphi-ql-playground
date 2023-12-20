e038fe7519a27113a7bc14ecaed57c269c33b9ab
Here East <nastiapiven@gmail.com>
2023-12-20 14:58:17 +0100

    feat: add test for Button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom";
 
-import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 import { Button } from "..";
 
 jest.mock("../../services/firebase", () => ({
@@ -9,10 +10,53 @@ jest.mock("../../services/firebase", () => ({
   },
 }));
 
+const handleClick = jest.fn();
+
 describe("Button component", () => {
   test("should be rendered with correct props", () => {
-    expect(true).toBe(true);
+    const props = {
+      type: "button",
+      name: "Click",
+      className: "button__test",
+    } as const;
 
-    render(<Button name="Click" />);
+    render(<Button {...props} />);
+
+    const button = screen.getByRole("button", { name: /click/i });
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass("button__test");
+    expect(button).toHaveAttribute("type", "button");
+  });
+
+  test("should call callback function if it's passed as a parameter", async () => {
+    const props = {
+      name: "Button",
+      onClick: (): void => handleClick(),
+    } as const;
+
+    render(<Button {...props} />);
+
+    const button = screen.getByRole("button", { name: /button/i });
+    expect(button).toBeInTheDocument();
+
+    await userEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalled();
+  });
+
+  test("should render children if there is a children prop", async () => {
+    render(
+      <Button name="Button">
+        {" "}
+        <h1>Child</h1>
+      </Button>,
+    );
+
+    const button = screen.getByRole("button", { name: /button/i });
+    expect(button).toBeInTheDocument();
+
+    const child = screen.getByText(/child/i);
+    expect(child).toBeInTheDocument();
   });
 });
