d4f8218880154d8905237efac29385c256dec26f
Here East <nastiapiven@gmail.com>
2023-12-20 15:48:45 +0100

    feat: add test for FooterCard

--- src/components/FooterCard/FooterCard.test.tsx
+++ src/components/FooterCard/FooterCard.test.tsx
@@ -1,20 +1,30 @@
 import "@testing-library/jest-dom";
 
-// import user from "@testing-library/user-event";
-// import { render } from "@testing-library/react";
-// import { FooterCard } from "..";
+import { render, screen } from "@testing-library/react";
+import { FooterCard } from "..";
 
-// const teamMember = {
-//   name: "John Snow",
-//   image: "john-snow.jpg",
-//   github: "github.com/john-snow",
-//   description: "My name is John Snow",
-//   position: "Frontend Developer",
-// };
+jest.mock("../../services/firebase", () => ({
+  auth: {
+    getAuth: jest.fn(),
+  },
+}));
+
+const teamMember = {
+  name: "John Snow",
+  image: "/john-snow.jpg",
+  github: "github.com/john-snow",
+  description: "My name is John Snow",
+  position: "Frontend Developer",
+};
 
 describe("FooterCard component", () => {
-  test("should render correct team member's image, name and link.", () => {
-    // render(<FooterCard teamMember={teamMember} key="123" />);
-    expect(true).toBe(true);
+  test("should render correct team member's image and link.", () => {
+    render(<FooterCard teamMember={teamMember} key="123" />);
+
+    const image = screen.getByRole("img", { name: /Photo of John/i });
+    const link = screen.getByRole("link", { name: /John Snow/i });
+
+    expect(image).toBeInTheDocument();
+    expect(link).toBeInTheDocument();
   });
 });
