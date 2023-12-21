c78167927d9a33d3a65d9fecedefa3e0d77d5e98
Here East <nastiapiven@gmail.com>
2023-12-21 19:06:47 +0100

    feat: add test for validationSchema

---
+++ src/__test__/services/validationSchema.test.tsx
@@ -0,0 +1,34 @@
+import "@testing-library/jest-dom";
+
+import { LANGS } from "../../constants";
+import { generateLoginSchema, generateRegisterSchema } from "../../services";
+
+jest.mock("../../services/firebase", () => ({
+  auth: {
+    getAuth: jest.fn(),
+  },
+}));
+
+describe("generateLoginSchema function", () => {
+  test("should handle valid input for login schema", () => {
+    const schema = generateLoginSchema(LANGS.EN);
+    expect(schema.isValidSync({ email: "test@mail.com", password: "Aa1!aaaa" })).toBe(true);
+  });
+  test("should handle invalid input for login schema", () => {
+    const schema = generateLoginSchema(LANGS.EN);
+    expect(schema.isValidSync({ email: "invalid-email", password: "short" })).toBe(false);
+  });
+});
+
+describe("generateRegisterSchema function", () => {
+  test("should handle valid input for register schema", () => {
+    const schema = generateRegisterSchema(LANGS.EN);
+    expect(
+      schema.isValidSync({ name: "John Snow", email: "test@mail.com", password: "Aa1!aaaa" }),
+    ).toBe(true);
+  });
+  test("should handle invalid input for register schema", () => {
+    const schema = generateRegisterSchema(LANGS.EN);
+    expect(schema.isValidSync({ name: "", email: "invalid-email", password: "short" })).toBe(false);
+  });
+});
--- src/__test__/test.test.tsx
+++
@@ -1,7 +0,0 @@
-import "@testing-library/react";
-
-describe("Test", () => {
-  test("should run a test", async () => {
-    expect(true).toBe(true);
-  });
-});
