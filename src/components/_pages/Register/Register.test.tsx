83980c641ae4ddf329741d9333f412598bf8c7f7
Here East <nastiapiven@gmail.com>
2023-12-22 16:27:47 +0100

    feat: add test for Register

--- src/components/_pages/Register/Register.test.tsx
+++ src/components/_pages/Register/Register.test.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/router";
 import { AppContextProvider } from "../../../context";
 import { DICTIONARY } from "../../../constants";
 import { Page } from "../../../types";
-import { Login } from "../..";
+import { Register } from "../..";
 
 const mockAuthStateFalse = [null, false, null];
 
@@ -15,7 +15,7 @@ jest.mock("../../../services/firebase", () => ({
   auth: {
     getAuth: jest.fn(),
   },
-  loginUser: jest.fn(),
+  registerUser: jest.fn(),
 }));
 
 jest.mock("react-firebase-hooks/auth", () => ({
@@ -26,62 +26,72 @@ jest.mock("next/router", () => ({
   useRouter: jest.fn().mockReturnValue({ replace: jest.fn(), push: jest.fn() }),
 }));
 
-describe("Login component", () => {
-  test("should render 2 input fields, a button ad a title.", () => {
+describe("Register component", () => {
+  test("should render 3 input fields, a button ad a title.", () => {
     render(
       <AppContextProvider>
-        <Login />
+        <Register />
       </AppContextProvider>,
     );
 
-    const title = screen.getByRole("heading", { name: /hello/i });
+    const title = screen.getByRole("heading", { name: /register/i });
+    const inputName = screen.getByPlaceholderText(/name/i);
     const inputEmail = screen.getByPlaceholderText(/email/i);
     const inputPassword = screen.getByPlaceholderText(/password/i);
-    const button = screen.getByRole("button", { name: /log in/i });
+    const button = screen.getByRole("button", { name: /create account/i });
 
     expect(title).toBeInTheDocument();
+    expect(inputName).toBeInTheDocument();
     expect(inputEmail).toBeInTheDocument();
     expect(inputPassword).toBeInTheDocument();
     expect(button).toBeInTheDocument();
   });
 
-  test("should not show errors if email and password are correct.", async () => {
+  test("should not show errors if name, email and password are correct.", async () => {
+    const correctName = "John";
     const correctEmail = "test@mail.com";
     const correctPassword = "Aa1!aaaa";
 
     render(
       <AppContextProvider>
-        <Login />
+        <Register />
       </AppContextProvider>,
     );
 
-    const inputEmail: HTMLInputElement = screen.getByPlaceholderText(/email/i);
-    const inputPassword: HTMLInputElement = screen.getByPlaceholderText(/password/i);
+    const inputName = screen.getByPlaceholderText(/name/i);
+    const inputEmail = screen.getByPlaceholderText(/email/i);
+    const inputPassword = screen.getByPlaceholderText(/password/i);
 
+    await userEvent.type(inputName, correctName);
     await userEvent.type(inputEmail, correctEmail);
     await userEvent.type(inputPassword, correctPassword);
 
+    const errorName = screen.queryByText(DICTIONARY.en.formErrors.name_required);
     const errorEmail = screen.queryByText(DICTIONARY.en.formErrors.email_valid);
     const errorPassword = screen.queryByText(DICTIONARY.en.formErrors.password_required);
 
+    expect(errorName).not.toBeInTheDocument();
     expect(errorEmail).not.toBeInTheDocument();
     expect(errorPassword).not.toBeInTheDocument();
   });
 
   test("should redirect to Playground page on successful login.", async () => {
+    const correctName = "John";
     const correctEmail = "test@mail.com";
     const correctPassword = "Aa1!aaaa";
 
     render(
       <AppContextProvider>
-        <Login />
+        <Register />
       </AppContextProvider>,
     );
 
+    const inputName = screen.getByPlaceholderText(/name/i);
     const inputEmail = screen.getByPlaceholderText(/email/i);
     const inputPassword = screen.getByPlaceholderText(/password/i);
-    const button = screen.getByRole("button", { name: /log in/i });
+    const button = screen.getByRole("button", { name: /create/i });
 
+    await userEvent.type(inputName, correctName);
     await userEvent.type(inputEmail, correctEmail);
     await userEvent.type(inputPassword, correctPassword);
 
