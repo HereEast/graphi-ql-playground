81e8a31aa48512f8cbc3680e1d4bacec11327a09
Here East <nastiapiven@gmail.com>
2024-01-09 21:31:35 +0100

    feat: add variable and headers to request function

--- src/utils/makeRequest.ts
+++ src/utils/makeRequest.ts
@@ -1,10 +1,19 @@
-export async function makeRequest(URL: string, query: string): Promise<Response> {
+export async function makeRequest(
+  URL: string,
+  query: string,
+  variablesCode: string,
+  headersCode: string,
+): Promise<Response> {
   const res = await fetch(URL, {
     method: "POST",
     headers: {
       "content-type": "application/json",
+      ...(headersCode ? JSON.parse(headersCode) : {}),
     },
-    body: JSON.stringify({ query: query }),
+    body: JSON.stringify({
+      query: query,
+      variables: variablesCode ? JSON.parse(variablesCode) : {},
+    }),
   });
 
   return res;
