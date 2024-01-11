045505967013a5231f51e2d71f9c058174ec545e
Here East <nastiapiven@gmail.com>
2024-01-11 22:18:24 +0100

    feat: add Doc to Playground component, refactor

--- src/components/_pages/Playground/Playground.tsx
+++ src/components/_pages/Playground/Playground.tsx
@@ -1,26 +1,26 @@
 import { ChangeEvent, ReactElement, useState } from "react";
 
 import { useAppContext, useLocale } from "../../../hooks";
-import { Button, RequestEditor, ResponseEditor } from "../..";
+import { PLACEHOLDER_API } from "../../../constants";
+import { Button, RequestEditor, ResponseEditor, Doc } from "../..";
 
 import styles from "./Playground.module.scss";
 
 function Playground(): ReactElement {
-  const docsDisabled = true;
-
   const { apiEndpoint, setApiEndpoint } = useAppContext();
   const { tooltips, playground } = useLocale();
 
   const [value, setValue] = useState(apiEndpoint);
+  const [docOpened, setDocOpened] = useState(false);
 
   function handleInputChange(e: ChangeEvent<HTMLInputElement>): void {
     setValue(e.target.value);
-    // localStorage.setItem("endpoint", e.target.value);
   }
 
   function saveEndpoint(): void {
     if (apiEndpoint !== value) {
       setApiEndpoint(value);
+      localStorage.setItem("endpoint", value);
     }
   }
 
@@ -31,11 +31,13 @@ function Playground(): ReactElement {
           <input
             value={value}
             onChange={handleInputChange}
+            onClick={() => setDocOpened(false)}
+            onBlur={saveEndpoint}
             className={styles.playground__form_input}
+            placeholder={PLACEHOLDER_API}
           />
           <Button
-            name="Save"
-            onClick={saveEndpoint}
+            name={playground.saveButton}
             className={(styles.playground__form_button, styles.playground__header_button)}
             type="button"
           />
@@ -44,10 +46,12 @@ function Playground(): ReactElement {
         <Button
           name={playground.docButton}
           className={styles.playground__header_button}
-          disabled={docsDisabled}
           tooltip={tooltips.docs}
+          onClick={() => setDocOpened(!docOpened)}
         />
       </header>
+
+      <Doc docOpened={docOpened} />
 
       <div className={styles.playground__views}>
         <RequestEditor />
