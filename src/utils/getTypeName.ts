e8eec9761475461a909563e0d36fb442373519e9
Here East <nastiapiven@gmail.com>
2024-01-11 21:58:19 +0100

    feat: create getTypeName function

---
+++ src/utils/getTypeName.ts
@@ -0,0 +1,27 @@
+import {
+  IntrospectionListTypeRef,
+  IntrospectionNamedTypeRef,
+  IntrospectionNonNullTypeRef,
+} from "graphql";
+
+type Type = IntrospectionNamedTypeRef | IntrospectionNonNullTypeRef | IntrospectionListTypeRef;
+
+export function getTypeName(type: Type): string | null {
+  if ("name" in type && type.name) {
+    return type.name;
+  }
+
+  if ("ofType" in type) {
+    if (type.kind === "NON_NULL") {
+      const typeName = getTypeName(type.ofType);
+      return `${typeName}!`;
+    }
+
+    if (type.kind === "LIST") {
+      const typeName = getTypeName(type.ofType);
+      return `[${typeName}]`;
+    }
+  }
+
+  return null;
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,5 @@ export * from "./handleAuthError";
 export * from "./prettifyCode";
 export * from "./makeRequest";
 export * from "./isValidJson";
+export * from "./fetchSchema";
+export * from "./getTypeName";
