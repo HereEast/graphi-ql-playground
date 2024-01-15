de0d1a0a96d4fdfdfca58dc5126b88d6cddf2b7f
Here East <nastiapiven@gmail.com>
2024-01-16 00:22:54 +0100

    feat: update test for Playground component

--- src/components/_pages/Playground/Playground.test.tsx
+++ src/components/_pages/Playground/Playground.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { ReactElement } from "react";
 
 import { AppContextProvider } from "../../../context";
@@ -11,6 +11,8 @@ jest.mock(
   "../../ResponseEditor/ResponseEditor",
   () => (): ReactElement => <div>Response Editor</div>,
 );
+jest.mock("../../Documentation/Documentation", () => (): ReactElement => <div>Documentation</div>);
+jest.mock("../../EndpointForm/EndpointForm", () => (): ReactElement => <div>Form</div>);
 
 jest.mock("../../../services/firebase", () => ({
   auth: {
@@ -19,7 +21,7 @@ jest.mock("../../../services/firebase", () => ({
 }));
 
 describe("Playground component", () => {
-  test("should render RequestEditor and ResponseEditor components", () => {
+  test("should render component correctly.", () => {
     render(
       <AppContextProvider>
         <Playground />
@@ -28,8 +30,31 @@ describe("Playground component", () => {
 
     const requestView = screen.getByText(/request/i);
     const responseView = screen.getByText(/response/i);
+    const documentation = screen.getByText(/documentation/i);
+    const form = screen.getByText(/form/i);
+    const button = screen.getByRole("button", { name: /doc/i });
 
     expect(requestView).toBeInTheDocument();
     expect(responseView).toBeInTheDocument();
+    expect(documentation).toBeInTheDocument();
+    expect(form).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+
+  test("should toggle Doc on button click.", () => {
+    render(
+      <AppContextProvider>
+        <Playground />
+      </AppContextProvider>,
+    );
+
+    const button = screen.getByRole("button", { name: /doc/i });
+
+    expect(button).toBeInTheDocument();
+    expect(button).not.toHaveClass("docButton_open");
+
+    fireEvent.click(button);
+
+    expect(button).toHaveClass("docButton_open");
   });
 });
